b06be4a5c4cbf634d068802678326785e59e18a0
nishinishi9999 <nishinishi9999@gmail.com>
2017-01-17 02:11:01 +0100

    Create screenshare.js

---
+++ screenshare/js/screenshare.js
@@ -0,0 +1,32 @@
+// screenshare
+
+function set_video(stream)
+    {
+        var video = $('video')[0];
+        
+        video.srcObject        = stream;
+        video.onloadedmetadata = function(e) { video.play(); };
+    }
+
+function log_error(err)
+    {
+        console.log("Error:");
+        console.log('a'+err);
+    }
+
+
+//---- Execution -----------------------------------------------------------------------------------------------------
+
+var constraints =
+    {
+        audio: true,
+        video:
+            {
+                width      : 1280,
+                height     : 720,
+                mediaSource: 'window'
+            }
+    }
+
+var promise = navigator.mediaDevices.getUserMedia(constraints).then(set_video).catch(log_error);
+console.log(promise);
